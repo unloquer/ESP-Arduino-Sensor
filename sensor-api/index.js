2c36d9cd65135cec89e4ba53a87dfcb7dbeaac3f
Santiago Gaviria <sgaviria@gmail.com>
2017-07-27 00:40:28 -0500

    api changes

--- sensor-api/index.js
+++ sensor-api/index.js
@@ -16,6 +16,12 @@ app.use(bodyParser.text({ "type": "text/*", limit: "4mb" }));
 var timeout = require('connect-timeout')
 app.use(timeout('5m'))
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
