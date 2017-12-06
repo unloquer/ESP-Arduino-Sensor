61356268151e862d06afb67f6637b6874f384254
Santiago Gaviria <sgaviria@gmail.com>
2017-12-06 16:57:27 -0500

    resume work. remove old unused stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,212 +1,172 @@
-/*
- * CC 2016, Un/loquer - http://unloquer.org
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+#include "app.h"
 
-#include <Arduino.h>
+int INIT = 1;
+const int DEBUG = 0;
+const int READ_LOG = 1;
+const int DELETE_LOG = 1;
+const int SEND_LOG = 0;
+const int SEND_RECORD = 1;
 
-#include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-// #include <ESP8266mDNS.h>
+GPSData gps;
+DHT11Data dht11;
+PlantowerData plantower;
+PlantowerData plantowerData;
 
-// Custom helper libraries
-#include "config.h"
+#include <DoubleResetDetector.h>
 
-char ssid[] = "C3P";
-char password[] = "trespatios";
+// Number of seconds after reset during which a 
+// subseqent reset will be considered a double reset.
+#define DRD_TIMEOUT 100
 
-ESP8266WebServer server(80);
+// RTC Memory Address for the DoubleResetDetector to use
+#define DRD_ADDRESS 0
 
-const int led = 13;
-const String BASE_URL = "http://104.131.1.214:3000/api";
+DoubleResetDetector drd(DRD_TIMEOUT, DRD_ADDRESS);
 
-boolean signedIn = false;
-byte MAC[6];
+void readLog() {
+  Serial.println("Reading log ...");
+  File file = SPIFFS.open("log", "r");
+  String line = "";
+  if (!file) Serial.println("file open failed");  // Check for errors
+  while (file.available()) {
+    wdt_disable();
+    // Read all the data from the file and display it
 
-#include "templates.h"
+    char c = file.read();
+    if(c == '\r') {
+      if(!line.endsWith("NULL")) {
+        Serial.println("Posting: "+line);
+        if(SEND_RECORD) {
+          postCsv("http://45.55.34.88:3000/api/v0/air.csv", line);
+        }
+      }
 
-// Declare local functions
-boolean ackDevice();
-String macToString();
-void getRoot();
-void handleNotFound();
-void connectWifi(String ssid, String password);
+      // Discard the \n, which is the next byte
+      file.read();
+      line = "";
+    } else {
+      line += String(c);
+    }
+    wdt_enable(1000);
+  }
+}
 
-void getRoot() {
-  if(signedIn) {
+void deleteLog() {
+  Serial.println("Deleting log");
+  SPIFFS.remove("log");
+}
+
+void sendLog() {
+  String url = "http://192.168.0.18:3000/api/v0/air.csv";
+  postCsvFile(url, "log");
+}
+
+void syncLog() {
+  if(!INIT) { return; }
+  INIT = 0;
+
+  if(READ_LOG) {
+    readLog();
+  }
+
+  if(SEND_LOG) {
+    sendLog();
+  }
+
+  if(DELETE_LOG) {
+    deleteLog();
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\nStarting ...");
+
+  SPIFFS.begin();
+  setupLeds();
+  setupGPS();
+  setupPlantower();
+  setupDHT11();
+  
+  if (drd.detectDoubleReset()) {
+    Serial.println("Connecting to network ...");
+    setupWifi();
+    syncLog();
+  }
+
+}
+
+void loop() {
+  gps = getGPSData();
+  if(gps.ready) {
+    dht11 = getDHT11Data();
+    plantower = getPlantowerData();
+
+    if(plantower.ready) {
+      plantowerData = plantower;
+      save();
+    }
+
+    if(plantowerData.ready) {
+      ledParticulateQuality(plantowerData);
+    }
+  }
+
+  drd.loop();
+}
+
+void save() {
+  char filename [] = "log";                     // Assign a filename or use the format e.g. SD.open("datalog.txt",...);
+  File file = SPIFFS.open(filename, "a+");        // Open a file for reading and writing (appending)
+  if (!file) {
+    Serial.println("file open failed");   // Check for errors
     return;
   }
-  ackDevice();
+
+  String frame = csvFrame();
+  Serial.println(frame);
+  file.println(frame);
+  file.close();
 }
 
-// String representation of the MAC, without the colons
-String macToString() {
-  String mac;
-  for (int i = 0; i <= 5; i++) {
-    mac += String(MAC[i], HEX);
-  }
-  return mac;
-}
+String csvFrame() {
+  /* CSV is ordered as:
+   "id","lat","lng","date","time","altitude","course","speed","humidity",
+   "temperature","pm1","pm25","pm10"
+  */
 
-// Acknowledge this ESP by MAC Address
-boolean ackDevice() {
-  String mac = macToString();
+  // First datum is the sensor_id
+  String frame = SENSOR_ID + STR_COMMA;
 
-  JsonObject* ackDeviceJson;
-  // _getApi("/sensors?filter[where][mac][eq]="+mac, &ackDeviceJson);
-  return false;
-}
+  // Add GPS data
+  char strlat[25],strlng[25];
+  dtostrf(gps.lat, 3, 6, strlat);
+  dtostrf(gps.lng, 3, 6, strlng);
 
-boolean existsSensor(String sensorID) {
-  String mac = macToString();
+  frame += strlat + STR_COMMA;
+  frame += strlng + STR_COMMA;
+  frame += gps.date + STR_COMMA;
+  frame += gps.time + STR_COMMA;
+  frame += gps.altitude + STR_COMMA;
+  frame += gps.course + STR_COMMA;
+  frame += gps.speed + STR_COMMA;
 
-  /*const int bufferSize = JSON_OBJECT_SIZE(1);
-  StaticJsonBuffer<bufferSize> jsonBuffer;
-  JsonObject& exists = jsonBuffer.createObject();*/
-  // Response: { exists: true }
-  String url("/sensors/"+sensorID+"/exists");
-  // JsonObject *exists = _getApi(url);
-  // return exists["exists"];
-  return true;
-}
-
-boolean registerDevice() {
-  const int bufferSize = JSON_OBJECT_SIZE(1);
-  StaticJsonBuffer<bufferSize> resJsonBuffer;
-  JsonObject& res = resJsonBuffer.createObject();
-
-  // Build request body object
-  StaticJsonBuffer<bufferSize> dataJsonBuffer;
-  JsonObject& data = dataJsonBuffer.createObject();
-
-  // Response: { exists: true }
-  String url("/devices");
-  _postApi("/devices", data, res, 0);
-  if(data.containsKey("_id")) {
-
-  }
-}
-
-bool connectWifi(String ssid, String password) {
-  int retries = 0;
-  bool connected = false;
-  
-  WiFi.begin(ssid.c_str(), password.c_str());
-
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay(500);
-    Serial.print (".");
-    if(retries == 10) { break; }
-  }
-
-  if(retries < 10) { connected = true; }
-  return connected;
-}
-
-bool checkConfig() {
-  return true;
-}
-
-void registerBoard() {
-
-}
-
-void displayWifiSetup() {
-
-}
-
-void setup ( void ) {
-  // We need to first check whether this is
-  // a new or existing deployment.
-  //
-  // Primero chequeamos si este es un despliegue
-  // nuevo o uno que ya existe.
-  bool hasConfig = checkConfig();
-  if(hasConfig) {
-    WifiConfig wifi = getWifiConfig();
-    bool connected = connectWifi(wifi.ssid, wifi.password);
-
+  // Add DHT11 data
+  if(dht11.ready) {
+    frame += dht11.humidity + STR_COMMA;
+    frame += dht11.temperature + STR_COMMA;
   } else {
-    displayWifiSetup();
+    frame += STR_NULL + STR_COMMA + STR_NULL + STR_COMMA;
   }
 
-
-
-
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
-  Serial.begin(115200);
-
-  Serial.println ( "" );
-
-  // Wait for connection
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );
+  // Add Plantower data
+  if(plantower.ready) {
+    frame += plantower.pm1 + STR_COMMA;
+    frame += plantower.pm25 + STR_COMMA;
+    frame += plantower.pm10;
+  } else {
+    frame += STR_NULL + STR_COMMA + STR_NULL + STR_COMMA + STR_NULL;
   }
 
-  Serial.println ( "" );
-  Serial.print ( "Connected to " );
-  Serial.println ( ssid );
-  Serial.print ( "IP address: " );
-  Serial.println ( WiFi.localIP() );
-
-  /*if ( mdns.begin ( "esp8266", WiFi.localIP() ) ) {
-    Serial.println ( "MDNS responder started" );
-  }*/
-
-  // server.on ( "/", handleRoot );
-  // server.on ( "/test.svg", drawGraph );
-  server.on ( "/inline", []() {
-    server.send ( 200, "text/plain", "this works as well" );
-  });
-
-  server.onNotFound ( handleNotFound );
-  server.begin();
-  Serial.println ( "HTTP server started" );
-}
-
-JsonObject* renderConfigJson() {
-  const int bufferSize = JSON_OBJECT_SIZE(1);
-  StaticJsonBuffer<bufferSize> resJsonBuffer;
-  JsonObject &res = resJsonBuffer.createObject();
-  JsonObject *out = &res;
-  return out;
-}
-
-void loop ( void ) {
-  // mdns.update();
-  server.handleClient();
-}
-
-void handleNotFound() {
-  digitalWrite ( led, 1 );
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += ( server.method() == HTTP_GET ) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-
-  for ( uint8_t i = 0; i < server.args(); i++ ) {
-    message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
-  }
-
-  server.send ( 404, "text/plain", message );
-  digitalWrite ( led, 0 );
+  return frame;
 }
--- src/templates.h
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-#include "FS.h"
-
-String loginPage() {
-  char* html;
-  size_t size = 0;
-  snprintf(html, size, "");
-  return String(html);
-}
